ff27cc7570ca61b3104a205ec970a9dac79967fe
YLSphere <lai.myron@gmail.com>
2024-03-12 21:58:20 -0700

    added workflow and fixed methodology

--- scripts.js
+++ scripts.js
@@ -5,61 +5,32 @@ var btn_conc = document.querySelector(".btn_three");
 
 // Get the displayText element
 var methodologyText = document.getElementById("methodologyText");
-var resultsText = document.getElementById("resultsText");
 var conclusionText = document.getElementById("conclusionText");
 
 // Add click event listener to the button
 btn_method.addEventListener("click", function() {
     // Show text with animation
     methodologyText.style.display = "block";
-    resultsText.style.display = "none";
     conclusionText.style.display = "none";
     methodologyText.classList.add("fade-in");
 
     btn_method.style.background = "transparent";
     btn_method.style.color = "#fff";
 
-    btn_results.style.background = "#fff";
-    btn_results.style.color = "rgb(0, 189, 173)";
-
     btn_conc.style.background = "#fff";
     btn_conc.style.color = "rgb(0, 189, 173)";
 
-});
-
-btn_results.addEventListener("click", function() {
-
-    methodologyText.style.display = "none";
-    resultsText.style.display = "block";
-    conclusionText.style.display = "none";
-
-    
-
-
-    resultsText.classList.add("fade-in");
-
-    btn_method.style.background = "#fff";
-    btn_method.style.color = "rgb(0, 189, 173)";
-
-    btn_results.style.background = "transparent";
-    btn_results.style.color = "#fff";
-
-    btn_conc.style.background = "#fff";
-    btn_conc.style.color = "rgb(0, 189, 173)";
 });
 
 btn_conc.addEventListener("click", function() {
     // Show text with animation
     methodologyText.style.display = "none";
-    resultsText.style.display = "none";
     conclusionText.style.display = "block";
     conclusionText.classList.add("fade-in");
 
     btn_method.style.background = "#fff";
     btn_method.style.color = "rgb(0, 189, 173)";
 
-    btn_results.style.background = "#fff";
-    btn_results.style.color = "rgb(0, 189, 173)";
 
     btn_conc.style.background = "transparent";
     btn_conc.style.color = "#fff";
